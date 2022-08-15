1c490ee346cfc1729373578841209a2fcaae8465
Murat Sevil <msevill54@gmail.com>
2022-08-15 21:40:52 +0300

    V3

--- src/Day2GitGithubb.java
+++ src/Day2GitGithubb.java
@@ -13,6 +13,8 @@ göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak
             git push -u origin master
 Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
 kullanırız.
+
+Not:git log --online versiyonlarımdaki durumu gösterir.
  */
 
 }
