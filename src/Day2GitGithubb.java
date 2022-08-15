6a29f2dea65c0e4a4daf04d77eb3efeee0cb7a80
Murat Sevil <msevill54@gmail.com>
2022-08-15 21:16:43 +0300

    V3

--- src/Day2GitGithubb.java
+++ src/Day2GitGithubb.java
@@ -12,6 +12,7 @@ göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak
             git remote add origin adres
             git push -u origin master
 Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
-kullanırız
+kullanırız.
  */
+
 }
